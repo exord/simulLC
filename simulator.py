2ee61ee186880ae305151f242b25af47e1a00d24
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2019-08-06 17:52:14 -0300

    Preparation of parameter dict in simulator.py

--- simulator.py
+++ simulator.py
@@ -7,10 +7,11 @@ Created on Thu May 16 10:49:39 2019
 """
 import os
 import datetime
+import imp
 
 import pastis.AstroClasses as ac
 
-from . import parameters
+import parameters
 
 # Define directory were lightcurves will be saved
 home = os.getenv('HOME')
@@ -19,12 +20,37 @@ home = os.getenv('HOME')
 lcdir = os.path.join(home, 'ExP/pastisML/lcs/',
                      datetime.date.today().isoformat())
 
+# Read configuration file
+infile = 'test.py'
+inputfile = os.path.join(home, 'ExP/pastisML/', infile)
+
+inconfig = imp.load_source('inconfig', inputfile)
+
+# Prepare inputs for Parameters class
+priors = []
+params = []
+for obj in inconfig.objectdict:
+    for par in inconfig.objectdict[obj]:
+        if inconfig.objectdict[obj][par][1] == 0:
+            continue
+        else:
+            priors.append([inconfig.objectdict[obj][par][2][0],
+                          [inconfig.objectdict[obj][par][2][1],
+                           inconfig.objectdict[obj][par][2][2]]])
+            params.append('{}_{}'.format(obj, par))
+            
+   
+# Instatianate parameters
+mypars = parameters.Parameters(priors, params)
+
 # Define number of cases per scenario
 n = 10
 
-# Draw random parameters from priors, and scenarios ids
-ids, params = parameters.draw(n)
+# Draw random parameters from priors
+mypars.draw(n)
 
+""""
 for scenario in ids:    
     # Instantaniate a planetary system
     planet = ac.Planet
+"""
